9b3b86efd52b7092b4a45f8e407858279b977a80
Blazej <blachypk@gmail.com>
2017-02-23 21:10:15 +0100

    .gitignore

---
+++ hello_world_v1.s
@@ -0,0 +1,16 @@
+.data #poczatek danych
+napis: #etykieta napis widoczna tylko dla kodu programu (bez .global)
+  .string "hello world!\n" #napis widoczny pod adresem etykiety
+  len = . - napis #dlugosc napisu (. to aktualny adres w pamieci - koniec napisu)
+.text #kod pragramu
+.global _start #etykieta globalna
+_start: #zawartosc etykiety start
+  movl $4, %eax #movl-zapis do 32 bit rejestru, 4 w EAX(funkcje systemowe) to write,
+  movl $1, %ebx #1 w EBX(plik docelowy) to standardowe wyjscie,
+  movl $napis, %ecx #ECX - adres z danymi do wyswietlenia
+  movl $len, %edx #EDX - dlugosc napsiu
+  int $0x80 #wywolanie przerwania i realizacja czynnosci hello world
+
+  movl $1, %eax #exit z argumentem 0
+  movl $0, %ebx
+  int $0x80
