a9b419ac4478a455f324e9cfb91d6668b4d6beda
Blazej <blachypk@gmail.com>
2017-04-08 19:37:28 +0200

    Update asm_with_c_print.s

--- asm_with_c_print.s
+++ asm_with_c_print.s
@@ -10,6 +10,10 @@
     .float 20.5
   number4:
     .float 111.1
+  number5:
+    .double 1.45
+  number6:
+    .double 3.11
   format_str:
     .ascii "%d\n\0"
 
@@ -24,11 +28,19 @@ main:
   push number2
   call addTwoIntAndPrint 
   
-  #add two double and printf
+  #add two float and printf
   mov number4, %eax
   push %eax
   mov number3, %eax
   push %eax
+  call addTwoFloatAndPrint
+
+  #add two double and printf
+BREAK:
+  push number5+4
+  push number5
+  push number6+4
+  push number6
   call addTwoDoubleAndPrint
 
   call exit
